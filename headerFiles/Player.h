781c2eed4cb0f7bcd552375b4353dcf9e1a0fbae
613xav <xdarboux@gmail.com>
2025-11-22 22:16:37 -0500

    player header

--- headerFiles/Player.h
+++ headerFiles/Player.h
@@ -1,1 +1,40 @@
 #pragma once
+#include <string>
+#include <iostream>
+
+
+// Required enum
+enum class Side { top, bottom, left, right };
+
+class Player {
+
+    //class variables for player
+    std::string name;  
+    bool active;
+    int nRubies;
+    Side side;
+    bool displayEndOfGame; 
+    
+public:
+    // Player construction
+    Player(const std::string& playerName = "", Side playerSide = Side::top, 
+           bool isActive = true, int rubies = 0)
+        : name(playerName), active(isActive), nRubies(rubies), side(playerSide), 
+          displayEndOfGame(false) {}
+
+    // Public functions for player
+    std::string getName() const { return name; }
+    void setActive(bool isActive); 
+    bool isActive() const { return active; }
+    int getNRubies() const { return nRubies; } 
+    void addRubis(const Rubis& rubis);  // Need to include Rubis.h
+    
+    // Side methods
+    Side getSide() const { return side; }
+    void setSide(Side newSide) { side = newSide; }
+    
+    void setDisplayMode(bool endOfGame) { displayEndOfGame = endOfGame; }
+    
+    // Overloading cout for printing
+    friend std::ostream& operator<<(std::ostream& os, const Player& player);
+};
--- headerFiles/Rubis.h
+++ headerFiles/Rubis.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <string>
+#include <iostream>
+
+class RubisDeck;
+
+class Rubis {
+
+    private:
+        int valeur;
+
+        Rubis(int v);
+        
+        friend class RubisDeck;
+
+
+    public:
+
+        operator int() const;
+        friend std::ostream& operator<<(std::ostream& os, const Rubis& rubis);
+
+
+};
